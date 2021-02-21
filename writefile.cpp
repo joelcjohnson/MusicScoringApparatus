5fdb479891767b693a5dc21534d8ca476867029f
joelcjohnson <77872106+joelcjohnson@users.noreply.github.com>
2021-02-20 18:49:26 -0600

    Update writefile.cpp
    Just adding some comments

--- writefile.cpp
+++ writefile.cpp
@@ -16,11 +16,16 @@ extern QString filename;
 
 writefile::writefile()
 {
+    /*  Takes the Qstring of our filename and converts it to a std string format,  
+        then creates a file with that name.. the std::ios::app prevents the file from being
+        overwritten everytime we open it. which is good because we open it evry single time we write a new sample.
+    */
     std::ofstream file (filename.toStdString(), std::ios::app);
 
-    char note1;
-    char note2;
+    char note1;     //This char is for the letter of the note.
+    char note2;     //This char is for the octave of the note.
 
+    // Convert the octavevalue and notevalue into somthing we can easily read... chars will do the trick
     if (notevalue == 12){note1 = 'B';note2 = ' ';}
     if (notevalue == 11){note1 = 'A';note2 = '#';}
     if (notevalue == 10){note1 = 'A';note2 = ' ';}
@@ -33,6 +38,10 @@ writefile::writefile()
     if (notevalue ==  3){note1 = 'D';note2 = ' ';}
     if (notevalue ==  2){note1 = 'C';note2 = '#';}
     if (notevalue ==  1){note1 = 'C';note2 = ' ';}
-    file << note1 << note2 << " " << octavevalue << " " << volume << " " << timestamp << endl;
+    
+    /*  This line of code writes what we are recording in real time to the text document we named   
+        notice how I list the order note volume then timestamp.. later this will be different but now this is fine.      
+    */
+    file << note1 << note2 << " " << octavevalue << " " << volume << " " << timestamp << endl;  
 
 }
