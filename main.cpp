6d6fa06b4e9dc0ad654a1b28b6e40fa6fcd33f52
joelcjohnson <joel.c.johnson@ndus.edu>
2021-02-21 19:28:12 -0600

    Made some comments on the Main funtion. Also made some comments on both the button and text box funtions in the mainwindow.cpp object.

--- main.cpp
+++ main.cpp
@@ -3,11 +3,16 @@
 #include <sequencer.h>
 
 using namespace std;
-QString filename = "null";
+QString filename = "null"; //called null for testing reasons.
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    /* Our main funtion initialises the mainwindow object for our user input.
+     * Whether or not the sequecer is being used depends now now depends on
+     * the mainwindow. Therefore the sequencer is called by the mainwindow.
+     */
     MainWindow w;
     w.show();
     return a.exec();
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -10,22 +10,32 @@ MainWindow::MainWindow(QWidget *parent) :
 
 MainWindow::~MainWindow()
 {
-    delete ui;
+    delete ui;    //destroys object when done
 }
 
+/*Since we need to have some control over collecting information
+  pushing this button starts the allocation of data
+
+  (currently the number of samples taken is dependent on a variable in
+  the sequener this button can only start the data allocation and not end it.)
+
+  In the future this will be enterchanged with a start and end button.
+  */
 #include "sequencer.h"
 void MainWindow::on_pushButton_clicked()
 {
         sequencer();
         ui->lineEdit->setText("File Named");
-
 }
 
+
+/*This funtion sets our user inputed string as the name of our .txt file
+This updates everytime the text changes in the text box...
+*/
 extern QString filename;
 void MainWindow::on_lineEdit_textChanged(const QString &arg1)
 {
 
         filename = arg1;
-
-
+        //arg1 is a QString value that the text box outputs.
 }
